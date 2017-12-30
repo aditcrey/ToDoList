7ea054a0529411e459046c4c544c03d82c9a930e
Aditya Thakre <aditcrey@gmail.com>
2017-12-30 18:40:17 +0530

    overrode the stop() method in the Main class

--- src/com/aditcrey/todoList/Main.java
+++ src/com/aditcrey/todoList/Main.java
@@ -1,11 +1,14 @@
 package com.aditcrey.todoList;
 
+import com.aditcrey.todoList.datamodel.TodoData;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Main extends Application {
 
@@ -20,5 +23,16 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void stop() throws Exception {
+        try{
+            TodoData.getInstance().storeTodoItems(); //this will store the items into the txt file when we close the application
+
+        }catch(IOException e){
+            System.out.println(e.getMessage());
+
+        }
     }
 }
